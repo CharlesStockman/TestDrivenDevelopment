66177c03a04910bf7e56d48c60c9b0ef50c9d466
CharlesStockamn <chuck.stockman@yahoo.com>
2023-07-19 00:06:53 -0400

    Rearrange the directory to add more examples

---
+++ fizzbizz/src/test/java/EnvironmentTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EnvironmentTest {
+
+    @Test
+    public void isEnvironmentUsable() {
+        Assertions.assertTrue(true);
+    }
+}
