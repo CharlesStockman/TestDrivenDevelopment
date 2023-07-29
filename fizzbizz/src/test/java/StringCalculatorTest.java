2428174bc72ce2ea4c4c93fdce3b3c47d5e660b7
CharlesStockamn <chuck.stockman@yahoo.com>
2023-07-29 03:39:07 -0400

    Cleanup

--- fizzbizz/src/test/java/StringCalculatorTest.java
+++ fizzbizz/src/test/java/StringCalculatorTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.security.InvalidParameterException;
 import java.security.KeyStore;
 import java.util.HashMap;
@@ -9,84 +11,122 @@ import java.util.Map;
 public class StringCalculatorTest {
 
     @Test
-    public void handle_input_is_empty_string() {
+    public void handle_input_is_empty_string() throws IOException {
         StringCalculator stringCalculator = new StringCalculator();
         Assertions.assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
-    public void handle_input_is_comma() {
+    public void handle_input_is_comma() throws IOException  {
         StringCalculator stringCalculator = new StringCalculator();
         Assertions.assertEquals(0, stringCalculator.add(","));
     }
 
-    @Test void  handle_input_is_newline() {
+    @Test void  handle_input_is_newline() throws IOException  {
         StringCalculator stringCalculator = new StringCalculator();
-        Assertions.assertEquals(0, stringCalculator.add("\n"));
+        InvalidParameterException exception = Assertions.assertThrows(
+                InvalidParameterException.class, () -> stringCalculator.add("    2,3"));
+        Assertions.assertEquals(
+                exception.getMessage(),"Parameter numbers cannot have spaces at the beginning or end of the string");
     }
 
-    @Test void handle_input_has_spaces_and_comma() {
+    @Test
+    void handle_input_has_spaces_and_comma() throws IOException  {
         StringCalculator stringCalculator = new StringCalculator();
-        Assertions.assertEquals(0, stringCalculator.add("    ,     "));
+        InvalidParameterException exception = Assertions.assertThrows(
+                InvalidParameterException.class, () -> stringCalculator.add("    2,3"));
+        Assertions.assertEquals(
+                exception.getMessage(),"Parameter numbers cannot have spaces at the beginning or end of the string");
 
     }
 
     @Test
-    public void handle_input_has_2_commas_together() {
+    void handle_input_should_have_spaces_at_the_start_or_end() {
+        StringCalculator stringCalculator = new StringCalculator();
+        InvalidParameterException exception;
+
+        exception = Assertions.assertThrows(
+                InvalidParameterException.class, () -> stringCalculator.add("   2,3    "));
+        Assertions.assertEquals(
+                exception.getMessage(),"Parameter numbers cannot have spaces at the beginning or end of the string");
+
+        exception = Assertions.assertThrows(
+                InvalidParameterException.class, () -> stringCalculator.add("2,3    "));
+        Assertions.assertEquals(
+                exception.getMessage(),"Parameter numbers cannot have spaces at the beginning or end of the string");
+
+        exception = Assertions.assertThrows(
+                InvalidParameterException.class, () -> stringCalculator.add("    2,3"));
+        Assertions.assertEquals(
+                exception.getMessage(),"Parameter numbers cannot have spaces at the beginning or end of the string");
+
+    }
+
+
+    @Test
+    public void handle_input_has_2_commas_together() throws IOException {
         StringCalculator stringCalculator = new StringCalculator();
 
         InvalidParameterException exception = Assertions.assertThrows(
                 InvalidParameterException.class, () -> stringCalculator.add(",,"));
-        Assertions.assertEquals(exception.getMessage(), "The 1st parameter cannot have a comma and no digit after");
+        Assertions.assertEquals(exception.getMessage(), "Parameter numbers cannot start or end with a comma");
     }
 
     @Test
-    public void handle_input_has_2_commas_together_with_valid_numbers() {
+    public void handle_input_has_2_commas_together_with_valid_numbers() throws IOException  {
         StringCalculator stringCalculator = new StringCalculator();
-        Assertions.assertEquals(5, stringCalculator.add("2,,3"));
+        InvalidParameterException exception = Assertions.assertThrows(
+                InvalidParameterException.class, () -> stringCalculator.add("2,,3"));
+        Assertions.assertEquals(
+                exception.getMessage(), "Invalid number -- <empty string>");
     }
 
     @Test
-    public void handle_input_is_one_number() {
+    public void handle_input_is_one_number() throws IOException  {
         StringCalculator stringCalculator = new StringCalculator();
         Assertions.assertEquals(4, stringCalculator.add("4"));
     }
 
     @Test
-    public void handle_input_is_one_number_and_comma() {
+    public void handle_input_is_one_number_and_comma() throws IOException  {
         StringCalculator stringCalculator = new StringCalculator();
         InvalidParameterException exception;
 
         exception = Assertions.assertThrows(
-                InvalidParameterException.class, () -> stringCalculator.add("1,2,"));
-        Assertions.assertEquals(exception.getMessage(), "The 1st parameter cannot have a comma and no digit after");
+                InvalidParameterException.class, () -> stringCalculator.add("1,"));
+        Assertions.assertEquals(
+                exception.getMessage(), "Parameter numbers cannot start or end with a comma");
 
         exception = Assertions.assertThrows(
-                InvalidParameterException.class, () -> stringCalculator.add("1,2,   "));
-        Assertions.assertEquals(exception.getMessage(), "The 1st parameter cannot have a comma and no digit after");
+                InvalidParameterException.class, () -> stringCalculator.add("1,"));
+        Assertions.assertEquals(
+                exception.getMessage(), "Parameter numbers cannot start or end with a comma");
     }
 
     @Test
-    public void handle_input_is_comma_and_one_number() {
+    public void handle_input_is_comma_and_one_number() throws IOException  {
         StringCalculator stringCalculator = new StringCalculator();
-        Assertions.assertEquals(4, stringCalculator.add(",4"));
+        InvalidParameterException exception = Assertions.assertThrows(
+                InvalidParameterException.class, () -> stringCalculator.add(",4"));
+        Assertions.assertEquals(
+                exception.getMessage(), "Parameter numbers cannot start or end with a comma");
     }
 
     @Test
-    public void handle_input_is_two_number() {
+    public void handle_input_is_two_number() throws IOException  {
         StringCalculator stringCalculator = new StringCalculator();
         Assertions.assertEquals(10, stringCalculator.add("4,6"));
     }
 
 
     @Test
-    public void handle_input_separator_are_both_comma_and_newline() {
+    public void handle_input_separator_are_both_comma_and_newline() throws IOException {
         StringCalculator stringCalculator = new StringCalculator();
         Assertions.assertEquals(21, stringCalculator.add("1,2\n3,4,5\n6"));
     }
 
     @Test
-    public void handle_input_both_are_invalid() {
+    public void handle_input_both_are_invalid() throws IOException {
         StringCalculator stringCalculator = new StringCalculator();
         InvalidParameterException invalidParameterException = Assertions.assertThrows(
                 InvalidParameterException.class, () -> (new StringCalculator()).add("dog,cat"));
@@ -94,7 +134,7 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void handle_input_some_inputs_are_invalid() {
+    public void handle_input_some_inputs_are_invalid() throws IOException {
         StringCalculator stringCalculator = new StringCalculator();
         InvalidParameterException invalidParameterException = Assertions.assertThrows(
                 InvalidParameterException.class, () -> (new StringCalculator()).add("dog,cat,1,3,bird"));
@@ -102,35 +142,35 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void handle_input_where_both_number_are_negative() {
+    public void handle_input_where_both_number_are_negative() throws IOException {
         StringCalculator stringCalculator = new StringCalculator();
         Assertions.assertEquals(-7, stringCalculator.add("-5,-2"));
     }
 
     @Test
-    public void handle_input_where_1st_number_is_negative() {
+    public void handle_input_where_1st_number_is_negative() throws IOException {
         StringCalculator stringCalculator = new StringCalculator();
         Assertions.assertEquals(3, stringCalculator.add("5,-2"));
     }
 
     @Test
-    public void handle_input_where_2nd_number_is_negative() {
+    public void handle_input_where_2nd_number_is_negative() throws IOException {
         StringCalculator stringCalculator = new StringCalculator();
         Assertions.assertEquals(3, stringCalculator.add("5,-2"));
     }
 
     @Test
-    public void handle_inputs_is_null() {
+    public void handle_inputs_is_null() throws IOException {
         StringCalculator stringCalculator = new StringCalculator();
         NullPointerException nullPointerException = Assertions.assertThrows(NullPointerException.class,
                 () -> stringCalculator.add(null));
         Assertions.assertEquals(
                 nullPointerException.getMessage(),
-                "Cannot invoke \"String.length()\" because \"str\" is null");
+                "Parameter numbers cannot be null");
     }
 
     @Test
-    public void handle_input_variable_amount() {
+    public void handle_input_variable_amount() throws IOException {
         StringCalculator stringCalculator = new StringCalculator();
         for (Map.Entry<String, Integer> test : createMultipleInputArgs().entrySet()) {
             Assertions.assertEquals(test.getValue(), stringCalculator.add(test.getKey()));
@@ -145,7 +185,7 @@ public class StringCalculatorTest {
      *
      * @return   A list of 6 different to determine if the add() can accept multiple arguments.
      */
-    private Map<String, Integer> createMultipleInputArgs() {
+    private Map<String, Integer> createMultipleInputArgs() throws IOException {
         Map<String, Integer> inputs = new HashMap<>();
 
         inputs.put("0,1,2", 3);
