533e05baaa9204ee4f8934131f9cb4fcdd4e9583
CharlesStockman <chuck.stockman@yahoo.com>
2024-03-20 23:53:17 -0400

    Created a sort list of words based on the id

---
+++ pyrimidAccess/testProgram.py
@@ -0,0 +1,43 @@
+import unittest
+import workProgram
+from dataclasses import dataclass
+
+@dataclass
+class Word():
+    key: int
+    word: str
+
+class TestProgram(unittest.TestCase):
+
+    
+   def test_word_table_from_file(self):
+
+    result_list = []
+    # result_list.append(Word(3, "love"))
+    # result_list.append(Word(6, "computers"))
+    # result_list.append(Word(2, "dogs"))
+    # result_list.append(Word(4, "cats"))
+    # result_list.append(Word(1, "I"))
+    # result_list.append(Word(5, "you"))
+    
+    result_list.append(Word(1, "I"))
+    result_list.append(Word(2, "dogs"))
+    result_list.append(Word(3, "love"))
+    result_list.append(Word(4, "cats"))
+    result_list.append(Word(5, "you"))
+    result_list.append(Word(6, "computers"))
+
+
+            
+    actual_list = workProgram.word_table_from_file("data.txt")
+    #self.assertListEqual(result_list, actual_list)
+    
+    for index in range(0,6):
+        self.assertTrue(actual_list[index].key == result_list[index].key)
+        self.assertTrue(actual_list[index].word == result_list[index].word)     
+        
+if ( __name__ == "__main__"):
+    unittest.main()
+ 
+    
+    
---
+++ pyrimidAccess/workProgram.py
@@ -0,0 +1,30 @@
+import re
+from dataclasses import dataclass, field
+
+@dataclass(order=True)
+class Word():
+    sort_index: int = field(init=False, repr=True)
+    key: int = field(init=True, repr=True)
+    word: str
+    
+    def __post_init__(self):
+        self.sort_index = self.key
+
+def word_table_from_file(file_name:str) -> list[str] :
+    """_summary_
+
+    Args:
+        file_name (str): _description_  The file name that will contains the primary key and word for each row in the table
+
+    Returns:
+        list[str]: _description_    A sorted list ( sorted by key ) of rows with attributes key and word.
+    """    
+    
+    results = []
+    with open(file_name, "r") as file:
+        for line in file:
+            split_string = re.split(r"\s+",line )
+            results.append( Word(int(split_string[0]), split_string[1]))
+    
+    results.sort()
+    return results
