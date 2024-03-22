f53f8c61ab99dc9950a4b5abfbf3125899eebfb1
CharlesStockman <chuck.stockman@yahoo.com>
2024-03-22 02:08:09 -0400

    Change to Map since there were missing values

--- pyrimidAccess/testProgram.py
+++ pyrimidAccess/testProgram.py
@@ -2,64 +2,73 @@ import unittest
 import workProgram
 from dataclasses import dataclass
 
-@dataclass
-class Word():
-    key: int
-    word: str
 
 class TestProgram(unittest.TestCase):
+    
+    def test_drawpyramid(self):
+    
+      print("build pyramid")
+    
+      numberPerLine = 1
+      total = 1
+    
+      while(total <= 300 ):
+        for counter in range(numberPerLine):
+            print(total, " ", end="")
+            total = total + 1
+        print("\n")
+        numberPerLine = numberPerLine + 1
 
     
-   def test_word_table_from_file_sorted_by_id(self):
-
-    result_list = []
+    def test_word_table_from_file_sorted_by_id(self):
     
-    result_list.append(Word(1, "I"))
-    result_list.append(Word(2, "dogs"))
-    result_list.append(Word(3, "love"))
-    result_list.append(Word(4, "cats"))
-    result_list.append(Word(5, "you"))
-    result_list.append(Word(6, "computers"))
+        result_map = {}
+        result_map[1] = "I"
+        result_map[2] = "dogs"
+        result_map[3] = "love"
+        result_map[4] =  "cats"
+        result_map[5] = "you"
+        result_map[6] = "computers"
         
-    actual_list = workProgram.word_table_from_file_sorted_by_id("data.txt")
-    
-    for index in range(0,6):
-        self.assertTrue(actual_list[index].key == result_list[index].key)
-        self.assertTrue(actual_list[index].word == result_list[index].word)     
-        
-   def test_create_list_with_index_of_right_indexes(self):
+        actual_map = workProgram.word_table_from_file_sorted_by_id("data.txt")
+        self.assertDictEqual(actual_map, result_map)
+          
+    def test_create_list_with_index_of_right_indexes(self):
 
-    length_of_word_database = 10;
-    result_index_list = [0, 2, 5, 9]
+      length_of_word_database = 10;
+      result_index_list = [1, 3, 6, 10]
     
-    actual_index_list = workProgram.create_list_with_right_indexs(length_of_word_database)
-    self.assertListEqual(result_index_list, actual_index_list)
+      actual_index_list = workProgram.create_list_with_right_indexs(length_of_word_database)
+      self.assertListEqual(result_index_list, actual_index_list)
     
-   def test_create_string(self):
-    word_list = []
-    word_list.append(Word(1, "I"))
-    word_list.append(Word(2, "dogs"))
-    word_list.append(Word(3, "love"))
-    word_list.append(Word(4, "cats"))
-    word_list.append(Word(5, "you"))
-    word_list.append(Word(6, "computers"))
+    def test_create_string(self):
+
+      word_map = {}
+      word_map[1] = "I"
+      word_map[2] = "dogs"
+      word_map[3] = "love"
+      word_map[4] =  "cats"
+      word_map[5] = "you"
+      word_map[6] = "computers"
     
-    index_list = [0, 2, 5]
+      index_list = [1, 3, 6]
     
-    result_string = "I love computers"
-    actual_string = workProgram.create_sentence(word_list, index_list)
-    self.assertEqual(result_string, actual_string)
+      result_string = "I love computers"
+      actual_string = workProgram.create_sentence(word_map, index_list)
+      self.assertEqual(result_string, actual_string)
     
-   def do(self):
+    def do(self):
        filename = "data.txt"
        result_string = "I love computers"
        
        actual_stirng = workProgram.do(filename)
        self.assertEqual(result_string, actual_stirng)
+
+
+        
        
     
     
-                
 if ( __name__ == "__main__"):
     unittest.main()
     
--- pyrimidAccess/workProgram.py
+++ pyrimidAccess/workProgram.py
@@ -1,15 +1,6 @@
 import re
 from dataclasses import dataclass, field
 
-@dataclass(order=True)
-class Word():
-    sort_index: int = field(init=False, repr=False)
-    key: int = field(init=True, repr=True)
-    word: str
-    
-    def __post_init__(self):
-        self.sort_index = self.key
-
 def word_table_from_file_sorted_by_id(file_name:str) -> list[str] :
     """_summary_
     
@@ -19,17 +10,15 @@ def word_table_from_file_sorted_by_id(file_name:str) -> list[str] :
         file_name (str): The file name that will contains the primary key and word for each row in the table
 
     Returns:
-        list[str]: A sorted list ( sorted by key ) of rows with attributes key and word.
+        list[str]: A map where the key is the index and the word is the value 
     """    
     
     # Create a list of index,words pairs from the file contents
-    results = []
+    results = {}
     with open(file_name, "r") as file:
         for line in file:
             split_string = re.split(r"\s+",line )
-            results.append( Word(int(split_string[0]), split_string[1]))
-    
-    results.sort()
+            results[int(split_string[0])] = split_string[1]    
     return results
 
 def create_list_with_right_indexs(length_of_word_database:int) -> list[int]:
@@ -51,23 +40,19 @@ def create_list_with_right_indexs(length_of_word_database:int) -> list[int]:
         indexes.append(total)
         
         index = index + 1       
-        
-    # Convert the array into a base 0 index
-    indexes = [ value - 1 for value in indexes ]
-
     return indexes
 
 def create_sentence(words:list[dataclass], indexes:list[int]) -> str:
     """_summary_
 
     Args:
-        words (list[dataclass]): A list of index/words
-        indexes (list[int]): A list of indexes where each index will retrieve a different word from the words list
+        words (list[dataclass]): A map of index/words
+        indexes (list[int]): A list of indexes where each index will retrieve a different word from the words map
 
     Returns:
         str: The sentence the funtion produces.
     """    
-    result = [ words[index].word + ' ' for index in indexes ]
+    result = [ words[index] + ' ' for index in indexes ]
     result = ''.join(result).strip()
     return result
 
@@ -81,5 +66,11 @@ def do(filename:str) -> str:
         str: The sentecnec created
     """    
     word_table = word_table_from_file_sorted_by_id(filename)
+    indexes = create_list_with_right_indexs(len(word_table))
+    
     sentence = create_sentence(word_table, create_list_with_right_indexs(len(word_table)))
     return sentence
+
+if __name__ == "__main__":
+    print("sentence = ", do("./data2.txt"))
+    
